5c98a29092e734226f8a641dcbfbe84078541cdf
sunilskcj <sunilskcj@gmail.com>
2022-02-28 01:35:43 +0530

    service done

---
+++ src/service/product.service.ts
@@ -0,0 +1,7 @@
+import { Products } from "src/models/products";
+import { List } from "src/repository/productlist";
+
+
+export class ProductService{
+    public  getProducts = () : Products[] => List  
+}
