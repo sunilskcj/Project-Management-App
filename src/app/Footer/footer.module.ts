8aca8ea9ec630ff8b8335a70bd6291cecf64e46f
sunilskcj <sunilskcj@gmail.com>
2022-02-28 01:35:54 +0530

    Footer design

---
+++ src/app/Footer/footer.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+import { FooterComponent } from "./footer.component";
+
+
+@NgModule({
+    declarations : [FooterComponent],
+    imports : [],
+    bootstrap:[FooterComponent],
+    exports : [FooterComponent]
+})
+
+export class Footer{}
